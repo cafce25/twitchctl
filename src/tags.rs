3fcfc0dc564fc1b47e4a2e55679953211ac3c8b8
Jonathan Birk <jona@jbirk.de>
2021-03-31 20:56:30 +0000

    Add a commandline interface and tags fetching

--- src/tags.rs
+++ src/tags.rs
@@ -1,8 +1,9 @@
 use super::api::ApiClient;
 use clap::{App, Arg, ArgMatches};
 use fuzzy_filter::FuzzyFilter;
+use twitch_api2::helix::tags::TwitchTag;
 
-pub fn get_tags_app() -> App<'static, 'static> {
+pub fn get_tags_app<'a>() -> App<'a, 'a> {
     App::new("tags")
         .about("manipulate a streams tags")
         .arg(
@@ -24,41 +25,59 @@ pub fn get_tags_app() -> App<'static, 'static> {
                         .help("filter tags with a fuzzy search"),
                 ),
         )
+        .subcommand(
+            App::new("list")
+                .arg(
+                    Arg::with_name("long")
+                        .short("l")
+                        .help("use a long listing format"),
+                )
+                .arg(
+                    Arg::with_name("broadcaster")
+                        .index(1)
+                        .required(true)
+                        .help("the broadcaster you want to fetch tags for"),
+                        //TODO default to the one running this command
+                )
+                .arg(
+                    Arg::with_name("needle")
+                        .index(2)
+                        .help("filter tags with a fuzzy search"),
+                ),
+        )
 }
 
-pub async fn tags(
-    client: ApiClient<'static>,
-    matches: &ArgMatches<'static>,
-) {
+pub async fn tags<'a>(client: ApiClient<'a>, matches: &ArgMatches<'a>) {
     match matches.subcommand() {
         ("listall", Some(sub_m)) => {
-            listall(
-                client,
+            let tags = client.get_all_tags().await.expect("valid tags");
+            list(
+                &tags,
                 matches.value_of("locale").unwrap(),
                 sub_m.value_of("needle"),
                 sub_m.is_present("long"),
             )
-            .await
         }
-        _ => {},
+        ("list", Some(sub_m)) => {
+            let tags = client.get_stream_tags(sub_m.value_of("broadcaster").unwrap().to_string()).await.expect("valid tags");
+            list(
+                &tags,
+                matches.value_of("locale").unwrap(),
+                sub_m.value_of("needle"),
+                sub_m.is_present("long"),
+            )
+        }
+        _ => {}
     }
 }
-
-async fn listall<'a>(
-    client: ApiClient<'static>,
-    locale: &'a str,
-    needle: Option<&str>,
-    long: bool,
-) {
-    //let locale = "en-us";
-    //let tags = client.get_all_tags().await.expect("valid tags");
+fn list<'a>(tags: &[TwitchTag], locale: &'a str, needle: Option<&str>, long: bool) {
     let filter = if let Some(needle) = needle {
         FuzzyFilter::new(needle)
     } else {
         FuzzyFilter::new("")
     };
     let max_len = tags
-        //.iter()
+        .iter()
         .map(|tag| {
             tag.localization_names
                 .get(locale)

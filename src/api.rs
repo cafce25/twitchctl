8e35ba316303761ba4fbbc49ec458afcc848ce3d
Jonathan Birk <jona@jbirk.de>
2021-04-07 21:08:40 +0000

    Add setting and adding of tags

--- src/api.rs
+++ src/api.rs
@@ -2,11 +2,15 @@ use std::error::Error;
 use twitch_api2::{
     helix::{
         search::{search_categories::Category, SearchCategoriesRequest},
-        streams::get_stream_tags::GetStreamTagsRequest,
+        streams::{
+            get_stream_tags::GetStreamTagsRequest,
+            replace_stream_tags::{ReplaceStreamTags, ReplaceStreamTagsRequest, ReplaceStreamTagsBody},
+        },
         tags::{GetAllStreamTagsRequest, TwitchTag},
-        users::GetUsersRequest,
+        users::{GetUsersRequest, User},
     },
     twitch_oauth2::{AccessToken, TwitchToken, UserToken},
+    types::{UserId, UserName},
     HelixClient,
 };
 use twitch_oauth2::client::surf_http_client;
@@ -17,6 +21,12 @@ use derivative::Derivative;
 enum ApiError {
     #[error("No user with login `{0}` found.")]
     NoUser(String),
+}
+
+pub enum UserIdent {
+    UserName(UserName),
+    UserId(UserId),
+    None,
 }
 
 async fn get_user(token_string: &str) -> Result<UserToken, Box<dyn Error + 'static>> {
@@ -52,6 +62,10 @@ impl<'a> ApiClient<'a> {
         self.token.login.as_ref()
     }
 
+    pub fn get_user_id(&self) -> &str {
+        &self.token.user_id
+    }
+
     pub async fn search_categories(
         &self,
         term: &str,
@@ -73,23 +87,48 @@ impl<'a> ApiClient<'a> {
             Ok(None)
         }
     }
-    pub async fn get_stream_tags(&self, login: &str) -> Result<Vec<TwitchTag>, Box<dyn Error>> {
-        let channel_info_req = GetUsersRequest::builder()
-            .login(vec![login.to_string()])
-            .build();
-        let channel_info_res = self
-            .helix_client
-            .req_get(channel_info_req, &self.token)
-            .await?;
-        if channel_info_res.data.len() == 0 {
-            return Err(Box::new(ApiError::NoUser(login.to_string())));
-        }
-        let tag_req = GetStreamTagsRequest::builder()
-            .broadcaster_id(channel_info_res.data[0].id.to_string())
-            .build();
+
+    pub async fn get_users(
+        &self,
+        user_names: &[&UserName],
+        user_ids: &[&UserId],
+    ) -> Result<Vec<User>, Box<dyn Error>> {
+        let user_names: Vec<UserName> = user_names.iter().map(|n| n.to_string()).collect();
+        let user_ids: Vec<UserName> = user_ids.iter().map(|n| n.to_string()).collect();
+        let req = match (user_names.len(), user_ids.len()) {
+            (0, 0) => GetUsersRequest::builder().build(),
+            (_, 0) => GetUsersRequest::builder().login(user_names).build(),
+            (0, _) => GetUsersRequest::builder().id(user_ids).build(),
+            _ => GetUsersRequest::builder()
+                .id(user_ids.into())
+                .login(user_names.into())
+                .build(),
+        };
+
+        let res: Vec<User> = self.helix_client.req_get(req, &self.token).await?.data;
+        Ok(res)
+    }
+
+    pub async fn replace_stream_tags(
+        &self,
+        broadcaster_id: String,
+        tag_ids: Vec<String>,
+    ) -> Result<ReplaceStreamTags, Box<dyn Error + 'static>> {
+        let req = ReplaceStreamTagsRequest::builder().broadcaster_id(broadcaster_id).build();
+        let body = ReplaceStreamTagsBody::builder().tag_ids(tag_ids).build();
+        let res = self.helix_client.req_put(req, body, &self.token).await?;
+        Ok(res)
+    }
+
+    pub async fn get_stream_tags(
+        &self,
+        id: String,
+    ) -> Result<Vec<TwitchTag>, Box<dyn Error>> {
+        let tag_req = GetStreamTagsRequest::builder().broadcaster_id(id).build();
         let tag_res = self.helix_client.req_get(tag_req, &self.token).await?;
         Ok(tag_res.data)
     }
+
     pub async fn get_all_tags(&self) -> Result<Vec<TwitchTag>, Box<dyn Error>> {
         let mut tags = vec![];
         let mut pagination = None;
@@ -106,5 +145,27 @@ impl<'a> ApiClient<'a> {
             }
         }
         Ok(tags)
+    }
+
+    pub async fn get_broadcaster_id(
+        &self,
+        broadcaster_ident: UserIdent,
+    ) -> Result<UserId, Box<dyn Error>> {
+        match broadcaster_ident {
+            UserIdent::None => Ok(self.get_user_id().to_string()),
+            UserIdent::UserId(broadcaster_id) => Ok(broadcaster_id),
+            UserIdent::UserName(broadcaster_name) => {
+                match self.get_users(&[&broadcaster_name], &[]).await {
+                    Ok(userlist) => {
+                        if userlist.is_empty() {
+                            Err(Box::new(ApiError::NoUser(broadcaster_name)))
+                        } else {
+                            Ok(userlist[0].id.clone())
+                        }
+                    }
+                    Err(e) => { Err(e) }
+                }
+            }
+        }
     }
 }
